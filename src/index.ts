39f8fff7eb01ac37259b44c11b3d9872448b44e0
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-04-22 20:45:33 +0200

    detect what case a file supports

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,65 @@
-export const hello = (name: string): string => `Hello ${name}`;
+import * as fs from 'fs';
+import { camelCase, pascalCase, kebabCase } from 'change-case';
 
+const ignoredDirectoryNames: Array<string> = ['.git', '.github', 'node_modules'];
+const fileExtensionsToConsider = ['ts', 'js', 'tsx', 'jsx', 'snap'];
+const fileExtensionsToDrop = ['test', 'tests', 'spec', 'specs'].concat(fileExtensionsToConsider);
 
-export const asyncFunction = async (input: string, success: boolean = true) => {
-  return new Promise<string>((resolve, reject) => {
-    if(success){
-      resolve(`${input} resolve`);
-      return;
+enum Case {
+    Pascal,
+    Camel,
+    Kebab
+}
+
+class CaseReport {
+    constructor(public directory: string, public filenameWithExtensions: string, public fileName: string, public cases: Array<Case>) {
     }
+}
 
-    reject(new Error('rejected as requested'))
-  });
-};
+export function filenameWithoutExtension(segment: string): string {
+    let withoutExtensions = segment;
+    for (let i: number = 0; i < fileExtensionsToDrop.length; ++i) {
+        const extension = fileExtensionsToDrop[i];
+        if (withoutExtensions.endsWith('.' + extension)) {
+            // start at the beginning of the array again
+            i = -1;
+            withoutExtensions = withoutExtensions.substring(0, withoutExtensions.length - (1 + extension.length));
+        }
+    }
+    return withoutExtensions;
+}
 
-export const callbackFunction = (callback: (value: string) => void, invoke: boolean = true): void => {
-  if (invoke) {
-    callback('hello from the callback');
-  }
+export const walk = (dir: string): Array<CaseReport> => {
+    let results: Array<CaseReport> = [];
+    const list = fs.readdirSync(dir);
+
+    list.forEach(function (segment) {
+        const fullPath = dir + '/' + segment;
+        const stat = fs.statSync(fullPath);
+        if (stat && stat.isDirectory()) {
+            if (ignoredDirectoryNames.includes(segment)) {
+                return;
+            }
+            results = results.concat(walk(fullPath));
+        } else if (stat && stat.isFile()) {
+            if (fileExtensionsToConsider.some(extension => segment.endsWith(`.${extension}`))) {
+                let withoutExtensions = filenameWithoutExtension(segment);
+
+                const cases: Array<Case> = [];
+                if (withoutExtensions === camelCase(withoutExtensions)) {
+                    cases.push(Case.Camel);
+                }
+                if (withoutExtensions === pascalCase(withoutExtensions)) {
+                    cases.push(Case.Pascal);
+                }
+                if (withoutExtensions === kebabCase(withoutExtensions)) {
+                    cases.push(Case.Kebab);
+                }
+                results.push(new CaseReport(dir, segment, withoutExtensions, cases));
+            }
+        } else {
+            throw new Error(fullPath + ' is not file nor a directory');
+        }
+    });
+    return results;
 };
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,28 @@
-import {camelCase} from 'change-case';
+import { camelCase } from 'change-case';
+import { filenameWithoutExtension, walk } from '../src/index';
 
 describe('hello world', () => {
-  it('should greet the parameter', () => {
-    expect(camelCase('you-jump')).toEqual('youJump');
-  });
+    it('should greet the parameter', () => {
+        expect(camelCase('you-jump')).toEqual('youJump');
+    });
+});
+
+describe('filenameWithoutExtension', () => {
+    it('should drop single extension', () => {
+        expect(filenameWithoutExtension('juhu.ts')).toEqual('juhu');
+    });
+    it('should drop multiple extensions', () => {
+        expect(filenameWithoutExtension('juhu.test.ts')).toEqual('juhu');
+    });
+});
+
+describe('integration run', () => {
+    it('should ', () => {
+        let result = walk('.');
+        //result.forEach(console.log);
+        let noCaseDetected = result.filter(it => it.cases.length === 0);
+        console.log("total: "+ result.length);
+        console.log('no case: ' + noCaseDetected.length);
+        noCaseDetected.forEach(report => console.log(report.filenameWithExtensions));
+    });
 });
